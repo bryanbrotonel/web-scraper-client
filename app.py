1ff9857787da72104eecb5754e0fef9f005b7070
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-06-11 15:44:45 -0700

    Deployed craigslist spider to heroku

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from scrapinghub import ScrapinghubClient
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    apikey = '42ec2a2514134e4096d3b2ff6b65231d'
+    client = ScrapinghubClient(apikey)
+    job = client.get_job('527753/2/1')
+    df = pd.DataFrame(data=job.items.iter(), columns=['Title', 'Address', 'URL'])
+
+    return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
+
+if __name__ == "__main__":
+  app.run()
