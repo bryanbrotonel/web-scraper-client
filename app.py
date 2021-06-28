a9d8162b8d4b6a40eb2c52e4f93d6e3661e4a006
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-06-27 18:18:32 -0700

    Fresh Flask project

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template
 import requests
-from scrapinghub import ScrapinghubClient
 import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  response = requests.get(
-      'http://localhost:16210/crawl.json?spider_name=quotes&url=http://quotes.toscrape.com/').json()['items']
-  df = pd.DataFrame(data=response)
-
-  return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
+  return "Web Scraper Client"
 
 if __name__ == "__main__":
   app.run()
