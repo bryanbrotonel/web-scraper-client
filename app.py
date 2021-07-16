fed59fb1f4b9a470b593c817739326f7ad1ee0ea
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-07-15 18:13:05 -0700

    Added searchbar for amazon and craigslist

--- app.py
+++ app.py
@@ -1,43 +1,58 @@
-from flask import Flask, render_template
+# NOTE: source venv/bin/activate; FLASK_ENV=development flask run
+
+from flask import Flask, render_template, request
+from urllib.parse import quote
 import requests
 import pandas as pd
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
 
-  return "/amazon for Amazon Spider<br>/craigslist for Craigslist Spider<br>/ebay for Ebay Spider"
+    return render_template('search.html',)
+
+
+@app.route("/", methods=['POST'])
+def results():
+
+    search = request.form['search']
+
+    searchArgs = quote(search)
+
+    dfCraigslist = scarpe_craigslist(searchArgs)
+    dfAmazon = scrape_amazon(searchArgs)
+
+    df = dfCraigslist.append(dfAmazon)
+
+    return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
+
 
 @app.route("/ebay")
 def ebay():
 
-  arg = 'search'
-  argValue = 'Nintendo Switch Console'
+    arg = 'search'
+    argValue = 'Nintendo Switch Console'
 
-  request = requests.get(
-      'https://webscraper-server.herokuapp.com/crawl.json?spider_name=ebay&start_requests=True&crawl_args=%7B%22' + arg + '%22%3A%20' + argValue + '%7D').json()
-  df = pd.DataFrame(request['items'])
+    request = requests.get(
+        'https://webscraper-server.herokuapp.com/crawl.json?spider_name=ebay&start_requests=True&crawl_args=%7B%22' + arg + '%22%3A%20' + argValue + '%7D').json()
+    df = pd.DataFrame(request['items'])
 
-  return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
+    return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
 
-@app.route("/craigslist")
-def craigslist():
 
-  request = requests.get(
-      'https://webscraper-server.herokuapp.com/crawl.json?spider_name=craigslist&url=https://vancouver.craigslist.org/d/for-sale/search/sss?query=nintendo%20switch%20console&sort=rel').json()
-  df = pd.DataFrame(request['items'])
+def scarpe_craigslist(search):
+    request = requests.get(
+        'https://webscraper-server.herokuapp.com/crawl.json?spider_name=craigslist&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D').json()
+    return pd.DataFrame(request['items'])
 
-  return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
 
-@app.route("/amazon")
-def amazon():
+def scrape_amazon(search):
+    request = requests.get(
+        'https://webscraper-server.herokuapp.com/crawl.json?spider_name=amazon&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D').json()
+    return pd.DataFrame(request['items'])
 
-  request = requests.get(
-      'https://webscraper-server.herokuapp.com/crawl.json?spider_name=amazon&url=https://www.amazon.ca/Books-Last-30-days/s?rh=n%3A916520%2Cp_n_date%3A12035756011').json()
-  df = pd.DataFrame(request['items'])
-
-  return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
