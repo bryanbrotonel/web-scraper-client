abf93022a2b8ebc4b7e6f5508193374c0eb4c41f
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-08-12 15:28:39 -0700

    Removed logging

--- app.py
+++ app.py
@@ -22,19 +22,16 @@ def results():
 
     searchArgs = quote(search)
 
-    dfCraigslist = scarpe_craigslist(searchArgs)
-    dfAmazon = scrape_amazon(searchArgs)
-    dfEbay = scrape_ebay(searchArgs)
+    # dfCraigslist = scarpe_craigslist(searchArgs)
+    # dfAmazon = scrape_amazon(searchArgs)
+    # dfEbay = scrape_ebay(searchArgs)
 
-    df = dfCraigslist.append(dfAmazon).append(dfEbay)
+    df = scarpe_craigslist(searchArgs).append(scrape_amazon(searchArgs)).append(scrape_ebay(searchArgs))
 
     return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
 
 def scrape_ebay(search):
     ebayLink = 'https://webscraper-server.herokuapp.com/crawl.json?spider_name=ebay&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D'
-
-    print(ebayLink, file=sys.stderr)
-    print(ebayLink, file=sys.stdout)
 
     request = requests.get(ebayLink).json()
     return pd.DataFrame(request['items'])
@@ -43,18 +40,12 @@ def scrape_ebay(search):
 def scarpe_craigslist(search):
     craigslistLink = 'https://webscraper-server.herokuapp.com/crawl.json?spider_name=craigslist&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D'
 
-    print(craigslistLink, file=sys.stderr)
-    print(craigslistLink, file=sys.stdout)
-
     request = requests.get(craigslistLink).json()
     return pd.DataFrame(request['items'])
 
 
 def scrape_amazon(search):
     amazonLink = 'https://webscraper-server.herokuapp.com/crawl.json?spider_name=amazon&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D'
-
-    print(amazonLink, file=sys.stderr)
-    print(amazonLink, file=sys.stdout)
 
     request = requests.get(amazonLink).json()
     return pd.DataFrame(request['items'])
