497a2c39d6759e0ded4f748b407a7a43be5c9e1d
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-08-14 17:06:15 -0700

    ADded multiprocessing to requests

--- app.py
+++ app.py
@@ -4,76 +4,33 @@ from flask import Flask, render_template, request
 from urllib.parse import quote
 import requests
 import pandas as pd
-import sys
+from functools import partial
+import multiprocessing
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
 
     return render_template('search.html',)
 
-
 @app.route("/", methods=['POST'])
 def results():
-
     search = request.form['search']
-
     searchArgs = quote(search)
 
-    dfCraigslist = scarpe_craigslist(searchArgs)
-    dfAmazon = scrape_amazon(searchArgs)
-    dfEbay = scrape_ebay(searchArgs)
+    column_names = ['Product Name', 'Product Author', 'Product Price', 'Product Image']
+    df = pd.DataFrame(columns = column_names)
 
-    df = dfCraigslist.append(dfAmazon).append(dfEbay)
+    websites = ['amazon', 'ebay', 'craigslist']
 
-    return render_template('table.html',  tables=[df.to_html(index=False)], titles=df.columns.values)
+    pool = multiprocessing.Pool(processes=3)
+    results = pool.map(partial(scrape_data, search=searchArgs), websites)
 
-def scrape_ebay(search):
-    ebayLink = 'https://webscraper-server.herokuapp.com/crawl.json?spider_name=ebay&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D'
+    return render_template('table.html',  tables=[df.append([data for data in results]).to_html(index=False)], titles=df.columns.values)
 
-    request = None
-    while request is None:
-        try:
-            request = requests.get(ebayLink)
-        except requests.exceptions.Timeout:
-            continue
-
-    data = request.json()
-
-    return pd.DataFrame(data['items'])
-
-
-def scarpe_craigslist(search):
-    craigslistLink = 'https://webscraper-server.herokuapp.com/crawl.json?spider_name=craigslist&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D'
-
-    request = None
-    while request is None:
-        try:
-            request = requests.get(craigslistLink)
-        except requests.exceptions.Timeout:
-            continue
-
-    data = request.json()
-
-    return pd.DataFrame(data['items'])
-
-
-def scrape_amazon(search):
-    amazonLink = 'https://webscraper-server.herokuapp.com/crawl.json?spider_name=amazon&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D'
-
-    request = None
-    while request is None:
-        try:
-            request = requests.get(amazonLink)
-        except requests.exceptions.Timeout:
-            continue
-
-    data = request.json()
-
-    return pd.DataFrame(data['items'])
-
+def scrape_data(website, search):
+    return pd.DataFrame(requests.get('https://webscraper-server.herokuapp.com/crawl.json?spider_name=' + website + '&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D').json()['items'])
 
 if __name__ == "__main__":
     app.run()
