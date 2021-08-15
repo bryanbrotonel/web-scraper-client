39ef1890212a89bf83fbbd0ecf225271f901c60b
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-08-15 05:21:31 -0700

    Added app styling

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 import pandas as pd
 from functools import partial
 import multiprocessing
+import json
 
 app = Flask(__name__)
 
@@ -19,18 +20,21 @@ def results():
     search = request.form['search']
     searchArgs = quote(search)
 
-    column_names = ['Product Name', 'Product Author', 'Product Price', 'Product Image']
-    df = pd.DataFrame(columns = column_names)
+    websites = ['craigslist', 'ebay']
+    items = []
 
-    websites = ['amazon', 'ebay', 'craigslist']
-
-    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    pool = multiprocessing.Pool(processes=3)
     results = pool.map(partial(scrape_data, search=searchArgs), websites)
 
-    return render_template('table.html',  tables=[df.append([data for data in results]).to_html(index=False)], titles=df.columns.values)
+    for data in results:
+        items += data['items']
+
+    newlist = sorted(items, key=lambda k: k['Product Name']) 
+
+    return render_template('table.html',  data=newlist, search=search)
 
 def scrape_data(website, search):
-    return pd.DataFrame(requests.get('https://webscraper-server.herokuapp.com/crawl.json?spider_name=' + website + '&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D').json()['items'])
+    return requests.get('https://webscraper-server.herokuapp.com/crawl.json?spider_name=' + website + '&start_requests=true&crawl_args=%7B%22search%22%3A%20%22' + search + '%22%7D').json()
 
 if __name__ == "__main__":
     app.run()
---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+var typed = new Typed('.typed', {
+  strings: [
+    'Scrape the internet for <strong>Shoes</strong>.',
+    'Scrape the internet for <strong>Shirts</strong>.',
+    'Scrape the internet for <strong>Socks</strong>.',
+    'Scrape the internet for <strong>Basketballs</strong>.',
+    'Scrape the internet for <strong>Footballs</strong>.',
+    'Scrape the internet for <strong>Chairs</strong>.',
+  ],
+  typeSpeed: 120,
+  backSpeed: 60,
+  startDelay: 2400,
+  backDelay: 2400,
+  smartBackspace: true,
+  loop: true,
+});
